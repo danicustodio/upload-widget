abdb5aeb53d86567268b116b84367dbb86584780
Dani Custodio <dani.ps.custodio@gmail.com>
2025-01-22 21:54:22 -0300

    🔧 chore: add validation and global error handling

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,30 @@
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
+import { env } from '@/env'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: '*' })
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation Error',
+      issues: error.validation,
+    })
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
+
+server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
