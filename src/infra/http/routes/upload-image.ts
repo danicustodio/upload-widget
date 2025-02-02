98a1eabf7a282e630a16a43b0dd7ef27ced9f337
Dani Custodio <dani.ps.custodio@gmail.com>
2025-02-02 14:00:39 -0300

    📚 docs: add custom swagger transform function - config to accept multipart/form-data - custom transform function to support file upload

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,21 +1,29 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
+const ONE_MB = 1024 * 1024
+
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
     '/uploads',
     {
       schema: {
         summary: 'Upload an image',
-        body: z.object({
-          name: z.string(),
-        }),
+        consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
         },
       },
     },
     async (request, reply) => {
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: ONE_MB * 4, // 4MB
+        },
+      })
+
+      console.log(uploadedFile)
+
       return reply.status(201).send({ uploadId: 'test' })
     }
   )
